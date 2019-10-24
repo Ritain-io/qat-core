2f88edd7e1bd6b32ee4e4f00243670fa85ac87dd
Pedro Julio <pedro.julio@readinessit.com>
2019-10-24 17:29:09 +0100

    info code added

--- lib/qat/time.rb
+++ lib/qat/time.rb
@@ -84,7 +84,7 @@ module QAT
       #@return [ActiveSupport::TimeWithZone] Current time zone.
       def zone
         #self.zone = get_local_tz or 'UTC' unless ::Time.zone
-        log.debug "LOCAL TZZZZZZ: #{get_local_tz}"
+        log.info "LOCAL TZZZZZZ: #{get_local_tz}"
         self.zone = 'UTC'
         ::Time.zone
       end
